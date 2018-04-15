8a60f4f8de2c7e27c0883530c9f80f747989d62f
Danil Dream <33546123+danildream@users.noreply.github.com>
2018-04-15 22:48:12 +0300

    Сделал стр. - Статья

---
+++ article.php
@@ -0,0 +1,245 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Главная - Севастополь</title>
+
+    <!-- Bootstrap core CSS -->
+      <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="plugins/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="plugins/slick/slick-theme.css"/>
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+    <link href="media.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+<header class="container">
+    <nav class="navbar navbar-expand-md p-md-0 row">
+        <a class="logo col-6 col-sm-4 col-md-2 pl-md-0" href="#"> <img src="img/logo.png" alt=""></a>
+        <button class="navbar-toggler pr-3" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <img src="img/menu-button.png" alt="">
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav col-sm col-md-10">
+                <a class="nav-item nav-link col-sm" href="#">Главная</a>
+                <a class="nav-item nav-link col-sm" href="about.php">О проекте</a>
+                <a class="nav-item nav-link col-sm" href="#">Материалы</a>
+                <a class="nav-item nav-link col-sm" href="#">Эксперты</a>
+                <a class="nav-item nav-link col-sm" href="contacts.php#">Контакты</a>
+            </div>
+
+            <div class="col-sm col-md-2 p-0 my-sm-1 text-center">
+                <!-- // d-md-flex align-items-center p-0 row -->
+                <div class="row">
+                    <div class="col-md-8 col-sm-12 lang">
+                        <button class="btn btn-secondary dropdown-toggle h5 float-md-right" type="button"
+                                id="dropdownLangButton" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                            RU
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownLangButton">
+                            <div class="d-flex justify-content-center">
+                                <div class="item-lang"><a class="dropdown-item" href="#">EN</a></div>
+                                <div class="item-lang"><a class="dropdown-item" href="#">DE</a></div>
+                                <div class="item-lang"><a class="dropdown-item" href="#">FR</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12 pr-0 px-0 search">
+                        <button class="btn btn-secondary" type="button" data-toggle="collapse"
+                                data-target="#collapseSearch" aria-expanded="false">
+                            <img src="img/search.png" alt="">
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </nav>
+</header>
+
+<div class="collapse search" id="collapseSearch">
+    <div class="container">
+        <input class="form-control" placeholder="Введите запрос и нажмите Enter" aria-label="Search"
+           type="text">
+        <button class="btn btn-secondary" type="button" data-toggle="collapse"
+                data-target="#collapseSearch" aria-expanded="false">
+            <img src="img/close.png" alt="">
+        </button>
+    </div>
+</div>
+
+<main role="main">
+
+    <section class="article-wrapper">
+        <h1 class="article-title">Борьба демократических и олигархических тенденций как харизматическое лидерство</h1>
+        <p class="article-first-text">Социально-экономическое развитие интегрирует коммунизм. Христианско-демократический национализм представляет собой теоретический элемент политического процесса. Политическое учение Локка стабильно. </p>
+        <div class="row ml-0 mr-0">
+            <div class="col-md-4 pl-0 pr-0 article-date">20 июня 2018</div>
+            <div class="col-md-4 pl-0 pr-0 article-author">
+                Автор <a href="#">Иван Иванов</a>
+            </div>
+        </div>
+        <div class="article-text col-md-11 pr-0 pl-0">
+            <p>Социализм, с другой стороны, вызывает антропологический тоталитарный тип политической культуры. Теологическая парадигма однозначно сохраняет субъект власти, если взять за основу только формально-юридический аспект.</p>
+
+            <p>Политическое учение Монтескье приводит плюралистический феномен толпы, впрочем, не все политологи разделяют это мнение. Парадигма трансформации общества обретает эмпирический субъект власти, отмечает Б.Рассел. Либерализм представляет собой институциональный христианско-демократический национализм. Конечно, управление политическими конфликтами постоянно. Институциализация предсказуема.</p>
+
+            <p>Идеология, в первом приближении, теоретически символизирует бихевиоризм. Кризис легитимности существенно отражает институциональный субъект политического процесса. Политическая модернизация практически приводит системный феномен толпы. Понятие модернизации, как бы это ни казалось парадоксальным, отражает культ личности. Исходя из данного утверждения, политическое учение Н. Макиавелли фактически обретает политический процесс в современной России.</p>
+
+            <p>Авторитаризм, тем более в условиях социально-экономического кризиса, неизбежен. Теологическая парадигма, на первый взгляд, существенно приводит теоретический референдум, если взять за основу только формально-юридический аспект. Коллапс Советского Союза, как бы это ни казалось парадоксальным, однозначно вызывает социализм. Политическое учение Фомы Аквинского вызывает континентально-европейский тип политической культуры. Капиталистическое мировое общество теоретически представляет собой онтологический кризис легитимности.</p>
+
+            <p>Общеизвестно, что референдум существенно интегрирует субъект власти. Авторитаризм, особенно в условиях политической нестабильности, традиционно иллюстрирует референдум. Политическое учение Августина, с другой стороны, приводит элемент политического процесса, впрочем, это несколько расходится с концепцией Истона.</p>
+
+            <p>Конфедерация практически обретает механизм власти, о чем писали такие авторы, как Н.Луман и П.Вирилио. Политическая модернизация обретает функциональный референдум. Политическое учение Монтескье потенциально. Либерализм обретает референдум.</p>
+        </div>
+        <div class="soc-icons-block">
+            <div class="row ml-0 mr-0">
+                <div class="col-md-7">Понравилась статья? Не забудь поделиться</div>
+                <div class="ya-share2 col-md-5" data-services="collections,vkontakte,facebook,odnoklassniki,moimir,gplus" data-counter=""></div>
+            </div>
+        </div>
+    </section>
+
+    <!--    More Articles Block-->
+
+    <section class="more-articles">
+        <div class="container p-0">
+            <div class="row ml-0 mr-0 justify-content-between">
+                <div class="article-box">
+                    <a class="article-box__link" href="#"></a>
+                    <div class="card-image-box">
+                        <img class="card-img-top" src="img/news-2.jpg" alt="">
+                        <div class="card-img-bg"></div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Экзистенциальный англо-американский тип политической культуры</h5>
+                        <p class="card-text"></p>
+                        <p class="card-text"><a href="">10 июня 2018</a></p>
+                    </div>
+                </div>
+
+                <div class="article-box">
+                    <a class="article-box__link" href="#"></a>
+                    <div class="card-image-box">
+                        <img class="card-img-top" src="img/news-2.jpg" alt="">
+                        <div class="card-img-bg"></div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Экзистенциальный англо-американский тип политической культуры</h5>
+                        <p class="card-text"></p>
+                        <p class="card-text"><a href="">10 июня 2018</a></p>
+                    </div>
+                </div>
+
+                <div class="article-box">
+                    <a class="article-box__link" href="#"></a>
+                    <div class="card-image-box">
+                        <img class="card-img-top" src="img/news-2.jpg" alt="">
+                        <div class="card-img-bg"></div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Экзистенциальный англо-американский тип политической культуры</h5>
+                        <p class="card-text"></p>
+                        <p class="card-text"><a href="">10 июня 2018</a></p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <!--    More Articles Block End -->
+
+    <footer class="main-footer">
+        <div class="container">
+            <div class="row footer-nav">
+                <div class="footer-nav-left col-md col-lg-9 d-none d-md-block">
+                        <a class="footer-nav--item footer-nav--link col-md" href="#">Главная</a>
+                        <a class="footer-nav--item footer-nav--link col-md" href="#">О проекте</a>
+                        <a class="footer-nav--item footer-nav--link col-md" href="#">Материалы</a>
+                        <a class="footer-nav--item footer-nav--link col-md" href="#">Эксперты</a>
+                        <a class="footer-nav--item footer-nav--link col-md" href="#">Контакты</a>
+                </div>
+                <div class="footer-nav-right col-md col-lg-3 d-none d-md-block">
+                        <a class="footer-nav--item footer-nav--link col-md" href="#">СевГУ</a>
+                        <a class="footer-nav--item footer-nav--link col-md" href="#">НОЦ МПИБС</a>
+                </div>
+            </div>
+
+<!--            mobile footer menu-->
+
+            <div class="row footer-nav d-md-none">
+                    <a class="footer-nav--item footer-nav--link col-md" href="#">Главная</a>
+                    <a class="footer-nav--item footer-nav--link col-md" href="#">О проекте</a>
+                    <a class="footer-nav--item footer-nav--link col-md" href="#">Материалы</a>
+                    <a class="footer-nav--item footer-nav--link col-md" href="#">Эксперты</a>
+                    <a class="footer-nav--item footer-nav--link col-md" href="#">Контакты</a>
+                    <a class="feedback col-md" href="#">Обратная связь</a>
+                    <a class="footer-nav--item footer-nav--link col-md" href="#">СевГУ</a>
+                    <a class="footer-nav--item footer-nav--link col-md" href="#">НОЦ МПИБС</a>
+            </div>
+
+            <hr>
+            <div class="footer-info row">
+                <p class="copyright col-lg-5 col-md">© Геостратегический атлас Большого Средиземноморья, 2018</p>
+                <a class="feedback col-lg-2 col-md d-none d-md-block" href="#">Обратная связь</a>
+                <a class="developedby col-lg-5 col-sm" href="#">
+                    <img src="img/jm-logo.png" width="214" height="19" alt="">
+                </a>
+            </div>
+        </div>
+    </footer>
+
+</main><!-- /.container -->
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="plugins/jquery.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="plugins/bootstrap/bootstrap.min.js"></script>
+
+<!--    <script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.7.2/slick/slick.min.js"></script>-->
+<script src="plugins/slick/slick.js"></script>
+<script type="text/javascript">
+    $('.slider').slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 1,
+        centerMode: true,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+</script>
+
+<script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+<script src="//yastatic.net/share2/share.js"></script>
+
+
+</body>
+</html>
